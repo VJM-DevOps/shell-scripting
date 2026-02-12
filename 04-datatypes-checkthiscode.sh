c977266ec862d891619264f384b38a00727bf223
vijay john mengu <vijayjohnmengu@vijays-MacBook-Air.local>
2026-02-12 17:39:52 +0530

    shellscript

--- 04-datatypes-checkthiscode.sh
+++ 04-datatypes-checkthiscode.sh
@@ -3,10 +3,10 @@ NUM2=$2
 TIMESTAMP=$(date)
 echo "The script started execution at $TIMESTAMP"
 SUM=$((${NUM1}+${NUM2}))
-if [$? -ne 0 ]
+if [ $? -ne 0 ]
 then
     echo "please give run time values it takes 2 values: eg: $0 value1 value2"
     exit 1
 else
-echo "The sum of ${NUM1} and ${NUM2} is:: $SUM"
+    echo "The sum of ${NUM1} and ${NUM2} is:: $SUM"
 fi
