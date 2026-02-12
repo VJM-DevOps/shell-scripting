20181d5f78e50e1c1d77978d16a86b1d80ab1fce
vijay john mengu <vijayjohnmengu@vijays-MacBook-Air.local>
2026-02-12 17:36:01 +0530

    shellscript

--- 04-datatypes.sh
+++ 04-datatypes.sh
@@ -6,6 +6,7 @@ SUM=$((${NUM1}+${NUM2}))
 if [$? -ne 0 ]
 then
     echo "please give run time values it takes 2 values: eg: $0 value1 value2"
+    exit 1
 else
 echo "The sum of ${NUM1} and ${NUM2} is:: $SUM"
 fi
