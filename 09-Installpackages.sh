41e6f2b2269d3424e2ecd6d0d83fd5f9f25f8450
vijay john mengu <vijayjohnmengu@vijays-MacBook-Air.local>
2026-02-16 14:30:36 +0530

    shell-script

--- 09-Installpackages.sh
+++ 09-Installpackages.sh
@@ -28,23 +28,6 @@ else
     fi
 fi
 
-
-# dnf list installed msql-server
-# if [ $? -eq 0 ]
-# then
-#     echo -e " Mysql already $Y installed $N "
-#     exit 1
-# else
-#     dnf install mysql-server -y
-#     if [ $? -ne 0 ]
-#     then
-#         echo -e " Mysql installation.....$R Failuer $N "
-#         exit 1
-#     else
-#         echo -e " Mysql-server installation.....$G Success $N "
-#     fi
-# fi
-
 dnf list installed nginx
 if [ $? -eq 0 ]
 then
@@ -61,4 +44,23 @@ else
     fi
 fi
 
+
+
+
+
+# dnf list installed msql-server
+# if [ $? -eq 0 ]
+# then
+#     echo -e " Mysql already $Y installed $N "
+#     exit 1
+# else
+#     dnf install mysql-server -y
+#     if [ $? -ne 0 ]
+#     then
+#         echo -e " Mysql installation.....$R Failuer $N "
+#         exit 1
+#     else
+#         echo -e " Mysql-server installation.....$G Success $N "
+#     fi
+# fi
 
