fc6392a138a93acd13f8ec5e158ac9bf0fe6ee09
vijay john mengu <vijayjohnmengu@vijays-MacBook-Air.local>
2026-02-16 14:11:41 +0530

    shell-script:

---
+++ 09-Installpackages.sh
@@ -0,0 +1,47 @@
+#!/bin/bash
+
+R="/e[31m"
+Y="/e[32m"
+G="/e[33m"
+N="/e[0m"
+
+user=$(id -u)
+if [ $user -ne 0 ]
+then
+    echo -e "$R sudo access nedded to execute this script $N"
+    exit 1
+fi
+
+dnf list installed msql
+if [ $? -eq 0 ]
+then
+    echo -e " Mysql already $Y installed $N"
+    exit 1
+else
+    dnf install mysql
+    if [ $? -ne 0 ]
+    then
+        echo -e " Mysql installation.....$R Failuer $N "
+        exit 1
+    else
+        echo " Mysql installation..... $G Success $N "
+    fi
+fi
+
+dnf list installed nginx
+if [ $? -eq 0 ]
+then
+    echo " nginx already $Y installed $N "
+    exit 1
+else
+    dnf install nginx
+    if [ $? -ne 0 ]
+    then
+        echo " nginx installation.....$R Failuer $N "
+        exit 1
+    else
+        echo " nginx installation.....$G Success $N "
+    fi
+fi
+
+
